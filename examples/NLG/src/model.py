9d829dd38ae2232e4cbf21f1ddc09297e0560d72
harsh306 <harshnpathak@gmail.com>
2024-04-23 14:10:17 -0700

    encoder vectors

--- examples/NLG/src/model.py
+++ examples/NLG/src/model.py
@@ -256,7 +256,7 @@ class SparseEncoder(nn.Module):
     def __init__(self, dim, rank):
         super(SparseEncoder, self).__init__()
         self.lora_encoder = SparseLinear(dim, rank, sparsity=0.1)
-        self.lora_dropout = nn.Dropout(0.001)
+        self.lora_dropout = nn.Dropout(0.01)
 
     def forward(self, x):
         return self.lora_encoder(self.lora_dropout(x))
@@ -265,6 +265,7 @@ class SparseDecoder(nn.Module):
     def __init__(self, dim, rank):
         super(SparseDecoder, self).__init__()
         self.lora_decoder = SparseLinear(rank, dim, sparsity=0.1)
+        nn.init.zeros_(self.lora_decoder.lora_weight)
 
     def forward(self, x):
         return self.lora_decoder(x)
